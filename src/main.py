f2d55ffe7ad360a356b637945f8b56ecd4a69934
Asaf joseph <50167883+asafjo23@users.noreply.github.com>
2022-03-31 12:05:45 +0300

    add quick experiments switch option.

--- src/main.py
+++ src/main.py
@@ -40,7 +40,7 @@ import plotly.express as px
 DF_PATH = (
     f"{DATA_DIR}"
     f"/DEAM/annotations/annotations per each rater/"
-    f"song_level/static_annotations_songs_1_2000_standardized.csv"
+    f"song_level/static_annotations_songs_1_2000.csv"
 )
 
 Row = namedtuple("Row", "workerID SongId Valence Arousal Emotion")
@@ -72,55 +72,8 @@ def get_emotion(valence: int, arousal: int) -> str:
 
 
 if __name__ == "__main__":
-    columns = ["workerID", "SongId", "Valence"]
-    valence_original_df = read_csv(DF_PATH, skipinitialspace=True, usecols=columns)
-    valence_original_df.columns = ["user_id", "item_id", "rating"]
+    columns = ["workerID", "SongId", "Valence", "Arousal"]
+    original_df = read_csv(DF_PATH, skipinitialspace=True, usecols=columns)
+    print(original_df.Valence.mean())
 
-    valence_data_converter = DataConverter(
-        original_df=valence_original_df, n_random_users=0, n_ratings_per_random_user=0
-    )
 
-    columns = ["workerID", "SongId", "Arousal"]
-    arousal_original_df = read_csv(DF_PATH, skipinitialspace=True, usecols=columns)
-    arousal_original_df.columns = ["user_id", "item_id", "rating"]
-
-    arousal_data_converter = DataConverter(
-        original_df=arousal_original_df, n_random_users=0, n_ratings_per_random_user=0
-    )
-
-    valence_model = MF(
-        n_users=valence_data_converter.n_users,
-        n_items=valence_data_converter.n_item,
-        include_bias=True,
-    )
-
-    arousal_model = MF(
-        n_users=arousal_data_converter.n_users,
-        n_items=arousal_data_converter.n_item,
-        include_bias=True,
-    )
-
-    epochs = 100
-
-    criterion = MSELoss()
-    optimizer = SGD(valence_model.parameters(), lr=5, weight_decay=1e-3)
-    runner = Runner(
-        model=valence_model,
-        criterion=criterion,
-        optimizer=optimizer,
-        epochs=epochs
-    )
-
-    train_set = create_dataset(data_frame=valence_data_converter.encoded_df)
-    train_load = DataLoader(train_set, batch_size=1000, shuffle=True)
-    users, items, ratings = select_n_random(train_set)
-
-    with SummaryWriter("runs/DEAM/standardized/valence") as writer:
-        writer.add_graph(valence_model, (users, items))
-
-        for epoch in range(epochs):
-            epoch_loss = runner.train(train_loader=train_load, epoch=epoch, writer=writer)
-
-            print(f"epoch={epoch + 1}, loss={epoch_loss}")
-
-    torch.save(valence_model.state_dict(), f"{MODELS_DIR}/DEAM/standardized/valence.pt")
